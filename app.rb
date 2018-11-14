5ceb883d67d580b18fcd570a0ae59b9724588ab9
Jackwhitener <jackwhitener1@gmail.com>
2018-11-14 16:27:32 -0500

    Breaks them back into individual views. Oopsies.

--- app.rb
+++ app.rb
@@ -6,9 +6,28 @@ get '/' do
 end
 post '/number' do
 	num1 = params[:num1]
+	redirect 'number_2?num1=' + num1
+end
+get '/number_2' do
+	num1 = params[:num1]
+	erb :input2, locals: {num1: num1}
+end
+post '/number2' do
+	num1 = params[:num1]
+	num2 = params[:num2]
+	redirect 'operator?num1=' + num1 + '&num2=' + num2
+end
+get '/operator' do
+	num1 = params[:num1]
 	num2 = params[:num2]
 	op = params[:op]
-	redirect 'results?num1=' + num1 + '&op=' + op + '&num2='+ num2
+	erb :operator, locals: {num1: num1, num2: num2, op: op}
+end
+post '/oper_ator' do
+	num1 = params[:num1]
+	num2 = params[:num2]
+	op = params[:op]
+	redirect 'results?num1=' + num1 + '&num2=' + num2 + '&op=' + op
 end
 get '/results' do
 	num1 = params[:num1]
