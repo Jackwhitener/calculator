adc36abc43a2a66f231c1fb4d1a364009353091e
Jackwhitener <jackwhitener1@gmail.com>
2018-11-14 10:40:06 -0500

    Next post.

--- app.rb
+++ app.rb
@@ -6,7 +6,14 @@ get '/' do
 end
 post '/number' do
 	num1 = params[:num1]
+	redirect 'second_num?num1=' + num1
 end
-get 'number_2' do
-
+get 'second_num' do
+	num1 = params[:num1]
+	erb :input2, locals: {num1: num1}
+end
+post '/secondnum' do
+	num1 = params[:num1]
+	num2 = params[:num2]
+	redirect 'math?num1=' + num1 + '&num2=' + num2
 end
