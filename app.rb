3dd223325fe8fef0c1b77c76b48f79031a54d6d6
Jackwhitener <jackwhitener1@gmail.com>
2018-11-14 10:47:09 -0500

    Adds another get

--- app.rb
+++ app.rb
@@ -6,14 +6,18 @@ get '/' do
 end
 post '/number' do
 	num1 = params[:num1]
-	redirect 'second_num?num1=' + num1
+	redirect 'num2?num1=' + num1
 end
-get 'second_num' do
+get '/num2' do
 	num1 = params[:num1]
 	erb :input2, locals: {num1: num1}
 end
 post '/secondnum' do
+
+	redirect 'math?num1=' + num1 + '&num2=' + num2
+end
+get '/math' do
 	num1 = params[:num1]
 	num2 = params[:num2]
-	redirect 'math?num1=' + num1 + '&num2=' + num2
+	erb :operator, locals: {num1: num1, num2: num2}
 end
