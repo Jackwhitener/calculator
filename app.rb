012f11da59ff627b22c9659062820e7bc0aa6b7e
Jackwhitener <jackwhitener1@gmail.com>
2018-11-14 15:36:09 -0500

    Puts it all on one page, gives the radio buttons labels, and M O R E

--- app.rb
+++ app.rb
@@ -6,23 +6,23 @@ get '/' do
 end
 post '/number' do
 	num1 = params[:num1]
-	redirect 'num2?num1=' + num1
+	num2 = params[:num2]
+	op = params[:op]
+	redirect 'results?num1=' + num1 + '&op=' + op + '&num2='+ num2
 end
-get '/num2' do
-	num1 = params[:num1]
-	erb :input2, locals: {num1: num1}
-end
-post '/secondnum' do
+get '/results' do
 	num1 = params[:num1]
 	num2 = params[:num2]
-	redirect 'result?num1=' + num1 + '&num2=' + num2
-end
-get '/result' do
-	num1 = params[:num1]
-	num2 = params[:num2]
-	result_add = add(num1.to_i,num2.to_i)
-	result_subtract = subtract(num1.to_i,num2.to_i)
-	result_multiply = multiply(num1.to_i,num2.to_i)
-	result_divide = divide(num1.to_i,num2.to_i)
-	erb :result, locals: {num1: num1, num2: num2, result_add: result_add, result_subtract: result_subtract, result_multiply: result_multiply, result_divide: result_divide}
+	op = params[:op]
+	if op == "add"
+		result = add(num1.to_i,num2.to_i)
+		op = "\+"
+	elsif op == "-"
+		result = subtract(num1.to_i,num2.to_i)
+	elsif op == "*"
+		result = multiply(num1.to_i,num2.to_i)
+	elsif op == "/"
+		result = divide(num1.to_i,num2.to_i)
+	end
+	erb :results, locals: {num1: num1, num2: num2, result: result, op: op}
 end
--- math.rb
+++ math.rb
@@ -1,16 +1,20 @@
 def add(num1,num2)
 	result = num1+num2
-	return result
+	return result.to_s
 end
 def subtract(num1,num2)
 	result = num1-num2
-	return result
+	return result.to_s
 end
 def divide(num1,num2)
-	result = num1/num2
-	return result
+	if num2 == 0
+		return "stobbit"
+	else
+		result = num1/num2
+		return result.to_s
+	end
 end
 def multiply(num1,num2)
 	result = num1*num2
-	return result
+	return result.to_s
 end
